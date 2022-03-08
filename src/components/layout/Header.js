41ea1206787a50a3b7ea657a4e0ed7a8a607032f
Ashfak Niloy <ashfakniloy56@gmail.com>
2022-03-08 17:52:06 +0600

    fixed dropdown issue

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -15,17 +15,13 @@ const Header = () => {
           </Link>
         </div>
 
-        {!toggle ? (
-          <FaBars
-            className="md:hidden text-2xl hover:text-slate-400 duration-200"
-            onClick={() => setToggle(!toggle)}
-          />
-        ) : (
-          <FaTimes
-            className="md:hidden text-2xl hover:text-slate-400 duration-200"
-            onClick={() => setToggle(!toggle)}
-          />
-        )}
+        <div
+          className="md:hidden text-2xl hover:text-slate-400 duration-200"
+          onClick={() => setToggle(!toggle)}
+        >
+          <FaBars className={!toggle ? "block" : "hidden"} />
+          <FaTimes className={toggle ? "block" : "hidden"} />
+        </div>
 
         <div className="hidden md:flex gap-6 font-medium">
           <Link to="/" className="hover:text-slate-400 duration-200">
@@ -36,16 +32,29 @@ const Header = () => {
           </Link>
         </div>
       </div>
-      {toggle && (
-        <div className="flex flex-col md:hidden items-center py-6 text-slate-100 gap-6 font-medium">
-          <Link to="/" className="hover:text-slate-400 duration-200">
-            Home
-          </Link>
-          <Link to="/about" className="hover:text-slate-400 duration-200">
-            About
-          </Link>
-        </div>
-      )}
+
+      <div
+        className={
+          toggle
+            ? "flex flex-col md:hidden items-center py-6 text-slate-100 gap-6 font-medium"
+            : "hidden"
+        }
+      >
+        <Link
+          to="/"
+          className="hover:text-slate-400 duration-200"
+          onClick={() => setToggle(!toggle)}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="hover:text-slate-400 duration-200"
+          onClick={() => setToggle(!toggle)}
+        >
+          About
+        </Link>
+      </div>
     </div>
   );
 };
